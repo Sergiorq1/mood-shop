b65a54d49c1a15f1bf63e59f263f6447230c6caa
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-10-07 13:07:04 -0700

    displayed moods with button using javascript

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,31 +1,38 @@
-import data from './data.js'
-const itemsContainer = document.getElementById('items')
+import data from './data.js';
+
+const itemsContainer = document.getElementById('items');
 
 // the length of our data determines how many times this loop goes around
-for (let i=0; i<data.length; ++i) {
-    // create a new div element and give it a class name
-    let newDiv = document.createElement('div');
-    newDiv.className = 'item'
+data.forEach((mood) => {
+  // create a new div element and give it a class name
+  let newDiv = document.createElement('div');
+  newDiv.className = 'item';
 
-    // create an image element
-    let img = document.createElement('img');
-    // this will change each time we go through the loop. Can you explain why?
-    img.src = data[i].image
-    img.width = 300
-    img.height = 300
+  // create an image element
+  let img = document.createElement('img');
+  // this will change each time we go through the loop. Can you explain why?
+  img.src = mood.image;
+  img.width = 300;
+  img.height = 300;
 
-    // Add the image to the div
-    newDiv.appendChild(img)
-    // put new div inside items container
-    itemsContainer.appendChild(newDiv)
-    // create a paragraph element for a description
-    let desc = document.createElement('P')
-    // give the paragraph text from the data
-    desc.innerText =data[i].desc
-    // append the paragraph to the div
-    newDiv.appendChild(desc)
-    // do the same thing for price
-    let price = document.createElement('P')
-    price.innerText = data[i].price
-    newDiv.appendChild(price)
-}
+  let p_desc = document.createElement('P');
+  p_desc.innerText = mood.desc;
+
+  let p_price = document.createElement('P');
+  p_price.innerText = mood.price;
+
+  let button = document.createElement('button');
+  button.id = mood.name;
+  button.dataset.price = mood.price;
+  button.innerHTML = 'Add to Cart';
+
+  // Add the image to the div
+  newDiv.appendChild(img);
+  newDiv.appendChild(p_desc);
+  newDiv.appendChild(p_price);
+  newDiv.appendChild(button);
+
+  itemsContainer.appendChild(newDiv);
+});
+
+for (let i = 0; i < data.length; ++i) {}
